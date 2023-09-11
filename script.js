af484f9ac32db109f0b3e2e1eb16d37c8563b8be
mina <81285143+mkim41@users.noreply.github.com>
2023-09-10 21:57:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  $('.slide1').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev1',
+	nextArrow: '.arrow_next1',
+  });
+
+  $('.slide2').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev2',
+	nextArrow: '.arrow_next2',
+  });
+
+  $('.slide3').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev3',
+	nextArrow: '.arrow_next3',
+  });
+
+  $('.slide4').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev4',
+	nextArrow: '.arrow_next4',
+  });
+
+  $('.slide5').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev5',
+	nextArrow: '.arrow_next5',
+  });
+
+  $('.slide6').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev6',
+	nextArrow: '.arrow_next6',
+  });
+
+  $('.slide7').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev7',
+	nextArrow: '.arrow_next7',
+  });
+
+  $('.slide8').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev8',
+	nextArrow: '.arrow_next8',
+  });
+
+  $('.slide9').slick({
+	infinite: true,
+	prevArrow: '.arrow_prev9',
+	nextArrow: '.arrow_next9',
+  });
+});
